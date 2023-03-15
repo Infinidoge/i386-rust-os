6660ab4c84744687e055fb6e8af2415aa2494e19
Infinidoge <infinidoge@doge-inc.net>
2023-03-15 11:46:24 -0400

    kernel: attempt to use screen library

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,35 +1,57 @@
 #![no_std]
 #![no_main]
+#![allow(unused_imports)]
 
 use core::arch::asm;
+use i386_os::{
+    drivers::screen::{paint, print, COLS, ROWS, VIDEO_ADDRESS},
+    AsciiStr,
+};
 
-const ROWS: i32 = 25;
-const COLS: i32 = 80;
-
-static HELLO: &[u8] = b"Hello World!";
+const HELLO: AsciiStr = AsciiStr(b"Hello World!");
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    wait(1000); // Let screen 'chill' before doing anything
+    // Let screen 'chill' before doing anything
+    // For some reason if you try to write to the screen too quickly,
+    // The bootloader hasn't finished printing, so the text from the bootloader is still there
+    wait(1000);
 
-    let vga_buffer = 0xb8000 as *mut u8;
+    // Clear the screen:
+    // for x in 0..COLS {
+    //     for y in 0..ROWS {
+    //         paint((x, y).into(), ' ', 0x0f);
+    //     }
+    // }
+    // Currently disabled during testing, will be factored out
 
-    for i in 0..(ROWS * COLS) {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = b' ';
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;
-        }
-    }
-
+    // Print 'Hello World!' to the screen
     for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
+        paint((0, i), byte, 0x0b);
     }
+    // This works properly, it prints just fine
+
+    // (Attempt to) print 'Hello World!' to the screen
+    print(HELLO, 0x0b);
+    // This *does not* work properly, since `print` is imported from the srceen driver
+    // If the `print` use at the top is commented out, and the print function below
+    // (which is identical to the one in the module)
+    // is uncommented, it works just fine.
+    // WHY???
 
     loop {}
 }
+
+// pub fn print(string: AsciiStr, style: u8) {
+//     for (i, &byte) in string.0.iter().enumerate() {
+//         paint(
+//             // (min(pos.row + (i / COLS), ROWS), (pos.col + i) % COLS),
+//             (1, i),
+//             byte,
+//             style,
+//         );
+//     }
+// }
 
 fn wait(value: u32) {
     for _ in 0..value {
